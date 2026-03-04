8eadbf168845d8ff7994e0a905850745b66689f5
Neo Gonzales <gonzalesni@students.nu-clark.edu.ph>
2026-03-04 21:26:48 +0800

    Appointment system added

--- StockSense/StockSense/Data/ApplicationUser.cs
+++ StockSense/StockSense/Data/ApplicationUser.cs
@@ -5,9 +5,8 @@ namespace StockSense.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-        public string? FullName { get; set; }
-        public string? EmployeeId { get; set; }
-        public string? BranchLocation { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 
 }
